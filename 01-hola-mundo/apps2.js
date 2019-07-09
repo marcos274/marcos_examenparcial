f1ff6e80c97d70d30c4a8c23070749df675c7882
marcos274 <marcosriveros660@gmail.com>
2019-07-08 20:18:01 -0400

    first commit

---
+++ 01-hola-mundo/apps2.js
@@ -0,0 +1,10 @@
+function saludar(nombre) {
+    let mensaje = `hola ${nombre}`;
+
+    return mensaje;
+}
+
+let saludo = saludar('fernando');
+
+
+console.log(saludo);
---
+++ fundamentos/template-string.js/template-string.js
@@ -0,0 +1,6 @@
+let nombre = 'Deadpool';
+let real = 'Wade winston';
+
+
+console.log(nombre + ' ' + real);
+console.log(`${nombre}${real}`);
